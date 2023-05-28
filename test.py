98c3b06ce10356bbb81d7d0a38ae4b907f3a78e2
= <=>
2023-05-28 18:18:31 +0300

    Add some multithreading tests.

--- test.py
+++ test.py
@@ -6,8 +6,23 @@ import yt_ext
 
 
 import yt_ext_samples
+#yt_ext_samples.show_simple_playlist_info()
 
-yt_ext_samples.show_simple_playlist_info()
+import threading
 
-import AppKit
-AppKit.NSBeep()
+import time
+def task(item):
+    time.sleep(5)
+    print(item)
+
+items =['asdf','ffdsafs']
+
+threads=[]
+for item in items:
+    t = threading.Thread(target=task,args=(item,))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+print('done!')
